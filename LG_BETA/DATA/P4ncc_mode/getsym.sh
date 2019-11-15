87db5c494a8200a8930f0ee6c63f1ea6dde37ec7
Noe Mascello <noe.mascello@bluewin.ch>
2019-11-15 14:38:16 +0100

    data

--- LG_BETA/DATA/P4ncc_mode/getsym.sh
+++ LG_BETA/DATA/P4ncc_mode/getsym.sh
@@ -5,6 +5,6 @@ nmod=48
 for ((i=0;i<nmod;i++))
 do
         echo $i
-	vasp_findsym.sh --in=mode.vasp${i} --tol=1e-2
+	vasp_findsym.sh --in=mode.vasp${i} --tol=1e-4
         echo "_____________________________________"
 done
