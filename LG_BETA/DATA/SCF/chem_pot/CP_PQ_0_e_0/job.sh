796ef7ccf2da3046b2037345e6a6abcff717c9ca
Noe Mascello <noe.mascello@bluewin.ch>
2019-12-17 01:07:46 +0100

    data

---
+++ LG_BETA/DATA/SCF/chem_pot/CP_PQ_0_e_0/job.sh
@@ -0,0 +1,9 @@
+       #!/bin/bash
+       #BSUB -n 40
+       #BSUB -R "rusage[mem=3072]"
+       #BSUB -W 8:00
+       #BSUB -o chem_pot/CP_PQ_0_e_0.o
+       #BSUB -e chem_pot/CP_PQ_0_e_0.e
+       #BSUB -J chem_pot/CP_PQ_0_e_0
+
+        mpirun pw.x -npool 40 -in scf.in > scf.out
---
+++ LG_BETA/DATA/SCF/chem_pot/CP_Q_20_e_.20/job.sh
@@ -0,0 +1,9 @@
+       #!/bin/bash
+       #BSUB -n 40
+       #BSUB -R "rusage[mem=3072]"
+       #BSUB -W 8:00
+       #BSUB -o chem_pot/CP_Q_20_e_.20.o
+       #BSUB -e chem_pot/CP_Q_20_e_.20.e
+       #BSUB -J chem_pot/CP_Q_20_e_.20
+
+        mpirun pw.x -npool 40 -in scf.in > scf.out
